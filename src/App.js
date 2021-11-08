9e6bdb0d44355fdc77d6c647c66d83fcb293e065
angel daniel <daniel_1234@hotmail.es>
2021-11-08 15:29:40 -0400

    cambio de estructura en carpetas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import HeaderLinks from './components/HeaderLinks';
+import { ProtectedRouteAdmin } from './hoc/Permissions'
+
+// import { Auth } from 'aws-amplify';
 
 import {
-  BrowserRouter as Router,
+  BrowserRouter as Router, Switch, Route
 } from 'react-router-dom';
 
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports'; 
 // import aws_exports_prod from './aws-exports-prod'; 
 
-import App from './Components/App';
-
 Amplify.configure(aws_exports); 
 
-const AppWithRouter = () => (<Router><App /></Router>);
+const AppWithRouter = () => {
+
+  return  (
+            <Router>
+              <HeaderLinks/>
+              <Switch>
+                <Route exact path="/" render={() => <h1>test</h1>} />
+                <ProtectedRouteAdmin exact path="/" render={<h2>test2</h2>} props={{test: "test"}} />
+              </Switch>
+            </Router>
+          );
+};
 
 export default AppWithRouter;
