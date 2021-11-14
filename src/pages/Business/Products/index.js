58cf61753e01fe7ebe4171e416eaea006bf50e46
angel daniel <daniel_1234@hotmail.es>
2021-11-14 06:17:10 -0400

    agregando modal bootstrap

--- src/pages/Business/Products/index.js
+++ src/pages/Business/Products/index.js
@@ -1,6 +1,9 @@
-import React from "react";
-import { Container, Row } from "react-bootstrap";
+import { Button } from "@blueprintjs/core";
+import React, { useState } from "react";
+import { Container, Form, Modal, Row } from "react-bootstrap";
 import CustomTable from '../../../components/CustomTable/CustomTable'
+
+//import { ProductContext } from '../../providers/products/products.provider';
 
 const PRODUCTS_ITEMS = [
     { id: 1, name: 'shampoo', cost: '200', actions: null },
@@ -10,12 +13,63 @@ const PRODUCTS_ITEMS = [
 ];
 
 const Products = () => {
+    const [show, setShow] = useState(false);
+    const [edit, setEdit] = useState(false);
+    const [add, setAdd] = useState(false);
+
+    const [cost, setCost] = useState('');
+    const [qty, setQty] = useState('0');
+    const [productName, setProductName] = useState('');
+
+    const handleClose = () => setShow(false);
+    const handlen = () => {
+        setEdit(true);
+        setAdd(true);
+        setProductName(true);
+    }
+
+    //const { items, addItem } = useContext(ProductContext);
+    //addItem({ id: 1, name: 'shampoo' })
+
+    //console.log(items)
     return (
         <Container fluid>
             <h3 className="mt-5">Productos</h3>
+            <Button intent="Primary" onClick={(e) => { e.preventDefault(); setAdd(true); setShow(true); }} icon="add"></Button>
             <Row>
                 <CustomTable headers={['No.', 'Nombre', 'Costo', 'Accion']} items={PRODUCTS_ITEMS} />
             </Row>
+            {/* Modal para editar y ver detalle de productos */}
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{edit ? 'Editar Producto' : add ? 'Agregar Producto' : 'Ver Producto'}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Group controlId="name">
+                        <Form.Label>Nombre</Form.Label>
+                        <Form.Control readOnly={true} type="text" value={productName} />
+                    </Form.Group>
+                    <Form.Group controlId="cost">
+                        <Form.Label>Costo</Form.Label>
+                        <Form.Control readOnly={!edit && !add} type="number" value={cost} onChange={e => setCost(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group controlId="qty">
+                        <Form.Label>Cantidad</Form.Label>
+                        <Form.Control readOnly={!edit && !add} type="number" value={qty} onChange={e => setQty(e.target.value)} />
+                    </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Cerrar
+                    </Button>
+                    {(edit || add) &&
+                        <Button variant="primary" onClick={handlen}>
+                            Guardar
+                        </Button>
+                    }
+                </Modal.Footer>
+            </Modal>
+
         </Container>
     )
 }
